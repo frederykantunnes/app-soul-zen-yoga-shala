dbff2a0367095819a6993519b5aebb720b1a5fb3
Frederyk Antunnes <frederykantunnes@gmail.com>
2020-04-19 00:48:16 -0300

    Configure

--- lib/config/StringsConfig.dart
+++ lib/config/StringsConfig.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StringsConfig{
-  final urlApi = "https://app-soulzen.herokuapp.com/";
+  final urlApi = "https://api-souzenyogashala-production.herokuapp.com/";
   final ep_posts = "post/";
   final ep_agend = "agenda/";
   final ep_turma = "turma/";
   final ep_users = "usuarios/";
-  
-
 }
